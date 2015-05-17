95ae2efe7d94961c19e0bfaf35fcadd733f49a06
John Elash <elash@telus.net>
2015-05-17 10:21:18 -0700

    Continue with course progress

--- htdocs/comp1920/lesson04/receivefile.php
+++ htdocs/comp1920/lesson04/receivefile.php
@@ -1,5 +1,12 @@
 <?php
     move_uploaded_file($_FILES['userfile']['tmp_name'], "./received_file_dir/".$_FILES['userfile']['name']);
+    echo "<pre>" . "formatted text using pre<br />";
+    var_dump($_FILES);
+    echo "</pre>";
+
+    echo "non formatted text follows<br />";
+    var_dump($_FILES);
+
 ?>
 
 <!DOCTYPE html>
