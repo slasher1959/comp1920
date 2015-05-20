407ca1f1df944005d943b810c3f3362ca51630a4
John Elash <elash@telus.net>
2015-05-19 23:25:00 -0700

    more course progress

--- htdocs/comp1920/lesson04/lab10/read.php
+++ htdocs/comp1920/lesson04/lab10/read.php
@@ -1,5 +1,14 @@
 <?php
-    echo "in read.php<br />";
+/**
+ * read.php
+ *
+ * This script file is for lab10 in course COMP1920.
+ *
+ * Author:  John Elash
+ * Date:   May 19, 2015
+ *
+ */
+//    echo "in read.php<br />";
     $messageBoardFile = "messageboard.text";
     if (file_exists($messageBoardFile) !== true) {
         echo "doesn't exist";
@@ -11,24 +20,17 @@
     if (count($myFileArray) == 0) {
         echo "File: $messageBoardFile has no posts to display<br />";
     } else {
-        foreach ($myFileArray as $myFileArrayLine) {
+        foreach ($myFileArray as $myFileNdx=>$myFileArrayLine) {
             echo nl2br($myFileArrayLine);
-            echo nl2br("and another\n and still another\nyet another");
+//            $tempString = str_replace('and', 'BUT', $myFileArrayLine);
+//            echo nl2br($tempString);
+//            echo nl2br("and another\n and still another\nyet another") . "<br />";
+//            var_dump($myFileArrayLine);
+//            echo "<br />";
         }
     }
-    echo "<pre>"; 
-    var_dump($myFileArray); 
-    echo "</pre>";  
+//    echo "<pre>"; 
+//    var_dump($myFileArray); 
+//    echo "</pre>";  
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-    </head>
-    <body>
-        
-    </body>
-</html>
